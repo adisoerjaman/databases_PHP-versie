348e834ec52040429ce2df572fc5ba1cf6efef4f
Adi Soerjaman <adisoerjaman@gmail.com>
2023-04-17 10:19:11 +0000

    test 5

--- databases/test.sql
+++ databases/test.sql
@@ -5,7 +5,7 @@ USE `test`;
 CREATE TABLE `student` (
 `id` int AUTO_INCREMENT PRIMARY KEY, 
 `naam` text NOT NULL,
-`klas` varchar(16) NOT NULL,
+`klas` varchar(16) NOT NULL
 );
 
 INSERT INTO `student` (`id`, `naam`, `klas`) VALUES
