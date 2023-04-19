8f59e4de3199074bf7355e394c08260e6a777b3d
Adi Soerjaman <adisoerjaman@gmail.com>
2023-04-19 09:48:34 +0000

    made db jolanda and table

---
+++ databases/jolanda.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS `jolanda`;
+
+CREATE DATABASE `jolanda`;
+
+USE `jolanda`;
+
+CREATE TABLE `haarproducten` (
+`id` int AUTO_INCREMENT PRIMARY KEY, 
+`product` text NOT NULL,
+`prijs` int NOT NULL
+);
+
+INSERT INTO `haarproducten` (`id`, `product`, `prijs`) VALUES
+(1, 'Shampoo', '15'),
+(2, 'Conditioner', '20'),
+(3, 'Zilvershampoo', '12');
